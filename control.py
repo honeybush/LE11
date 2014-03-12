b01e58734201659c0cc3c20c24fe27b0a07d4111
Sequoia Hummingbird <sequoia_hummingbird@yahoo.com>
2014-03-12 23:18:35 +0800

    controlFix_01
    transferring of ingredients bought and food reduction somehow fixed

--- control.py
+++ control.py
@@ -3,10 +3,8 @@ import model
 
 class CC:
 	def __init__(self):
-		self.ing_list = {}
-		self.food_list = {}
-		self.ing_avi = {}
-		self.food_avi = {}
+		self.ing_list = []
+		self.food_list = []
 		self.order_f = {}
 		self.order_i = {}
 		self.cookOrder = {}
@@ -15,22 +13,35 @@ class CC:
 		self.ing_list = self.data.inglist
 	def getFood(self):
 		self.food_list = self.data.foodlist
-	def giveAviFood(self):
-		print "-" #if food_list value not 0 then add to list
-	def giveAviIng(self):
-		print "-" #if ing_list value not 0 then add to list
-	def buyIng(self):
+	def giveIng(self): #give the order of ingredients to model
+		k = self.order_i.keys()
+		for i in k:
+			for j in self.ing_list:
+				if k == j.name:
+					j.amt = j.amt + self.order_i.get(k)
+	def giveFood(self): #give the food cooked to model
+		print "-"
+	def getFood(self, n): #takes food from model. If not enough food, calls cook
+		k = self.order_i.keys()
+		for i in k:
+			for j in self.ing_list:
+				if k == j.name:
+					if n > j.amt:
+						self.cook(k)
+					else:
+						j.amt = j.amt - self.order_i.get(k)
+	def buyIng(self): #process stuff
 		self.data.inglist = self.ing_list
 		self.data.addIng(self.order_i.get("Ingredient"), self.order_i.get("Amount"))
 		self.getIng()
 		self.update_ing()
-	def cook(self):
-		print "-" #fix this with classes of food later
-	def update_resto(self):
+	def cook(self, food): #threading here. Cooks the food.
+		print "-"
+	def update_resto(self): #updates everything?
 		self.getIng()
 		self.getFood()
 		self.update_ing()
-	def update_ing(self):
+	def update_ing(self): #updates the text file for ingredients
 		keys = {}
 		keys = self.ing_list.keys()
 		f = open("ing.txt", "w")
@@ -38,5 +49,7 @@ class CC:
 			a = i + " = " + str(self.ing_list.get(i)) + "\n"
 			f.write(a)
 		f.close()
+	def update_food(self): #updates the text file for cooked food
+		print "-"
 
 cc = CC()
